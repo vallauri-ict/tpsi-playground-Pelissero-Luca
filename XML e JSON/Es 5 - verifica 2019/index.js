07060cbd4f0af9e1a2716a1a4064d9bafe5449e8
Pelissero-Luca <62608324+Pelissero-Luca@users.noreply.github.com>
2020-10-07 17:22:27 +0200

    Update index.js

--- XML e JSON/Es 5 - verifica 2019/index.js
+++ XML e JSON/Es 5 - verifica 2019/index.js
@@ -1,79 +1,71 @@
 "use strict"
 
-window.onload = function() {
-    let intestazioni = ["", "id", "name", "alcholic", "main ingredient", ""];
-    let larghezza = [40, 40, 60, 70, 70, 40];
-    let _table = document.getElementById("table");
+window.onload = function () {
+    let intestazioni = ["", "id", "name", "alcoholic", "main ingradient", ""];
+    let larghezze = [40, 40, 60, 70, 70, 40];
+    let _table = this.document.getElementById("table");
     let _listbox = document.getElementById("lstIngredienti");
 
     let _optTutti = document.getElementById("optTutti");
     let _optAlcolici = document.getElementById("optAlcoholic");
     let _optNonAlcolici = document.getElementById("optNonAlcoholic");
-
     _optTutti.addEventListener("click", caricaTabellaCocktails);
     _optAlcolici.addEventListener("click", caricaTabellaCocktails);
     _optNonAlcolici.addEventListener("click", caricaTabellaCocktails);
+
     _listbox.addEventListener("change", caricaTabellaCocktails);
 
-    caricaListaIntestazioni();
+    caricaListaIngredienti();
     caricaTabellaCocktails();
 
-    // *********************************************************************************
-    function creaIntestazioni() {
+    /************************************    FUNCTION    ************************************/
+    function creaIntestazione() {
         let _tr = document.createElement("tr");
         _table.appendChild(_tr);
-
         for (let i = 0; i < intestazioni.length; i++) {
             let _th = document.createElement("th");
             _th.innerHTML = intestazioni[i];
-            _th.style.width = larghezza[i] + "px";
+            _th.style.width = larghezze[i] + "px";
             _tr.appendChild(_th);
         }
     }
 
-    function caricaListaIntestazioni() {
-        // nel vettore json ingredients (enumerativo), accedo alla variabile ingredients
-        let vetIngredienti = ingredients.ingredients;
-        // let vetIngredienti = ingredients["ingredients"];
+    function caricaListaIngredienti() {
+        let vetIngradienti = ingredients.ingredients;
+        // let vetIngradienti=ingredients['ingredients'];
 
-        // ordinamento del vettore
-        vetIngredienti.sort(function(record1, record2) {
+        vetIngradienti.sort(function (record1, record2) {
             let str1 = record1.strIngredient1.toUpperCase();
             let str2 = record2.strIngredient1.toUpperCase();
-            if (str1 < str2)
-                return -1;
-            else if (str1 > str2)
-                return 1;
+            if (str1 < str2) return -1;
+            else if (str1 > str2) return 1;
             else return 0;
         });
+        console.log(vetIngradienti);
 
-        // caricamento dati nel listbox
         let _option = document.createElement("option");
-        _option.innerHTML = "";
+        _option.innerHtml = "";
         _listbox.appendChild(_option);
 
-        for (const item of vetIngredienti) {
+        for (const item of vetIngradienti) {
             let _option = document.createElement("option");
-            _option.innerHTML = item.strIngredient1;
-            _listbox.appendChild(_option);
+            _option.innerHTML = item["strIngredient1"];
+            _listbox.appendChild(_option)
         }
     }
 
     function caricaTabellaCocktails() {
         let vetCocktails = cocktails.drinks;
-
         _table.innerHTML = "";
-        creaIntestazioni();
-
+        creaIntestazione();
         for (const item of vetCocktails) {
-            if ((_optTutti.checked == true || (_optAlcolici.checked == true && item.strAlcoholic == "Alcoholic") || (_optNonAlcolici.checked == true && item.strAlcoholic == "Non alcoholic")) &&
-                (_listbox.value == "" || _listbox.value == item.strIngredient1)) {
+            if ((_optTutti.checked || (_optAlcolici.checked && item.strAlcoholic == "Alcoholic") || (_optNonAlcolici.checked && item.strAlcoholic == "Non alcoholic")) &&
+                (_listbox.value=="" || _listbox.value==item.strIngredient1)) {
                 let _tr = document.createElement("tr");
                 _table.appendChild(_tr);
-                let _td;
 
                 // immagine
-                _td = document.createElement("td");
+                let _td = document.createElement("td");
                 _tr.appendChild(_td);
                 let _img = document.createElement("img");
                 _td.appendChild(_img);
@@ -85,17 +77,17 @@ window.onload = function() {
                 _tr.appendChild(_td);
                 _td.innerHTML = item.idDrink;
 
-                // nome
+                // Name
                 _td = document.createElement("td");
                 _tr.appendChild(_td);
                 _td.innerHTML = item.strDrink;
 
-                // alchoolic
+                // Alcoholic
                 _td = document.createElement("td");
                 _tr.appendChild(_td);
                 _td.innerHTML = item.strAlcoholic;
 
-                // main ingredient
+                // Main ingradient
                 _td = document.createElement("td");
                 _tr.appendChild(_td);
                 _td.innerHTML = item.strIngredient1;
@@ -106,34 +98,31 @@ window.onload = function() {
                 let _a = document.createElement("a");
                 _td.appendChild(_a);
                 _a.href = "#";
-                _a.idDrinck = item.idDrink;
                 _a.innerHTML = "Dettagli";
+                _a.idDrink = item.idDrink;
                 _a.addEventListener("click", visualizzaDettagli);
             }
         }
-    }
+    };
 
     function visualizzaDettagli() {
         let _divDettagli = document.getElementById("dettagli");
-        // per non farlo concatenare
         _divDettagli.innerHTML = "";
 
         for (const item of cocktails.drinks) {
-            if (item.idDrinck == this.idDrinck) {
-
-                // h3
+            if (item.idDrink == this.idDrink) {
                 let _h3 = document.createElement("h3");
                 _h3.innerHTML = item.strDrink;
-                _divDettagli.appendChild(_h3);
 
-                // ingredients
-                let ingredient = "";
+                // h3
+                _divDettagli.appendChild(_h3);
+                let ingredients = "";
                 for (let i = 1; i <= 5; i++) {
-                    if (item["strIngredient" + i] != null)
-                        ingredient += item["strIngredient" + i] + " - ";
+                    if (item["strIngredient" + i] != null) {
+                        ingredients += item["strIngredient" + i] + " - ";
+                    }
                 }
                 let _pIngredients = document.createElement("p");
-                // _pIngredients.style.width = "100px";
                 _divDettagli.appendChild(_pIngredients);
                 _pIngredients.innerHTML = ingredients;
 
@@ -141,7 +130,7 @@ window.onload = function() {
                 let _img = document.createElement("img");
                 _divDettagli.appendChild(_img);
                 _img.src = item.strDrinkThumb;
-                _img.style.width = "140px"
+                _img.style.width = "140px";
 
                 break;
             }

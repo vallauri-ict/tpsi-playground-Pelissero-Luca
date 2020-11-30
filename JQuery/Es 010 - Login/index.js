b82083d2f4f5d3c211ace00694ce2a1c132e7123
Pelissero-Luca <62608324+Pelissero-Luca@users.noreply.github.com>
2020-11-30 17:03:34 +0100

    Updated

--- JQuery/Es 010 - Login/index.js
+++ JQuery/Es 010 - Login/index.js
@@ -1,10 +1,59 @@
+"use strict";
 
-var utenti = [ {"user":"pippo",  "pwd":"pwdPippo"},
-               {"user":"pluto",  "pwd":"pwdPluto"},
-			   {"user":"minnie", "pwd":"pwdMinnie"} ];
+var utenti = [{ "user": "pippo", "pwd": "pwdPippo" },
+    { "user": "pluto", "pwd": "pwdPluto" },
+    { "user": "minnie", "pwd": "pwdMinnie" }
+];
 
-			   
 $(document).ready(function() {
-   
-	
+
+    let _user = $("#txtUser");
+    let _pwd = $("#txtPwd");
+
+    let _msgUser = $("#msgUser")
+    let _msgPwd = $("#msgPwd")
+
+    _user.change(function() {
+        let ok = false;
+
+        if (_user.val() != "") {
+            for (const item of utenti) {
+                if (_user.val() == item.user) {
+                    ok = true;
+                }
+            }
+        }
+
+        if (!ok) {
+            _user.addClass("nok");
+            _msgUser.css({ color: "red" }).text("User non valido");
+            _msgUser.fadeIn(2000);
+        } else {
+            _user.removeClass("nok")
+            _msgUser.css({ color: "green" }).text("OK");
+            _msgUser.fadeIn(2000);
+        }
+    })
+
+    _user.hover(function() { $(this).addClass("over") }, function() { $(this).removeClass("over"); })
+    _pwd.hover(function() { $(this).addClass("over") }, function() { $(this).removeClass("over"); })
+
+    _pwd.change(function() {
+        let ok = false;
+
+        if (_pwd.val() != "")
+            for (const item of utenti)
+                if (_pwd.val() == item.pwd)
+                    if (item.user == _user.val())
+                        ok = true;
+        if (!ok) {
+            _pwd.addClass("nok");
+            _msgPwd.css({ color: "red" }).text("Pwd non valida o non corrispondente");
+            _msgPwd.fadeIn(2000);
+        } else {
+            _pwd.removeClass("nok")
+            _msgPwd.css({ color: "green" }).text("OK");
+            _msgPwd.fadeIn(2000);
+        }
+    })
 });

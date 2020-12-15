7ca9758ee24e35af3400d18fbf68915755d798a9
Luca <l.pelissero.1360@vallauri.edu>
2020-12-15 16:24:32 +0100

    Updated

--- JQuery/Es 013 - forza 4/index.js
+++ JQuery/Es 013 - forza 4/index.js
@@ -10,81 +10,81 @@ let turno = GIALLO;
 
 $(document).ready(function() {
 
-  let _wr = $("#wrapper");
-  let _header = $("#header");
+    let _wr = $("#wrapper");
+    let _header = $("#header");
 
-  // creazione pedine intestazione
-  for (var i = 0; i < COLONNE; i++) {
-    let pedina = $("<div>");
-    pedina.addClass("pedina");
-    pedina.appendTo(_header);
-    // pedina.hover(
-    //   function () { $(this).css({"background-color": turno}) },
-    //   function () { $(this).css({"background-color": GRIGIO}) }
-    // );
-  }
-
-  // utilizzo i delegated events perchè nel caso in cui passi più volte in quella parte di codice li accoda
-  // e poi i verificheranno più volte
-  _header.on("mouseenter", "div", function () { $(this).css({"background-color": turno}) })
-  _header.on("mouseleave", "div", function () { $(this).css({"background-color": GRIGIO}) })
-  _header.on("click", "div", down);
-
-  // creazione pedine wrapper
-  for (var i = 0; i < RIGHE; i++) {
-    for (var j = 0; j < COLONNE; j++) {
-      let pedina = $("<div>");
-      pedina.addClass("pedina");
-      pedina.appendTo(_wr);
-      pedina.prop("id", `btn-${i}-${j}`);
+    // creazione pedine intestazione
+    for (var i = 0; i < COLONNE; i++) {
+        let pedina = $("<div>");
+        pedina.addClass("pedina");
+        pedina.appendTo(_header);
+        // pedina.hover(
+        //   function () { $(this).css({"background-color": turno}) },
+        //   function () { $(this).css({"background-color": GRIGIO}) }
+        // );
     }
-  }
 
-  function down() {
-    // restituisce l'indice di $(this) all'interno del contenitore
-    let colonna = _header.children("div").index($(this));
-    let riga = RIGHE-1; // posizione della prima cella libera
+    // utilizzo i delegated events perchè nel caso in cui passi più volte in quella parte di codice li accoda
+    // e poi i verificheranno più volte
+    _header.on("mouseenter", "div", function() { $(this).css({ "background-color": turno }) })
+    _header.on("mouseleave", "div", function() { $(this).css({ "background-color": GRIGIO }) })
+    _header.on("click", "div", down);
 
+    // creazione pedine wrapper
     for (var i = 0; i < RIGHE; i++) {
-      let p = $(`#btn-${i}-${colonna}`);
-      if (p.css("background-color") != GRIGIO) {
-        riga = i-1;
-        break;
-      }
+        for (var j = 0; j < COLONNE; j++) {
+            let pedina = $("<div>");
+            pedina.addClass("pedina");
+            pedina.appendTo(_wr);
+            pedina.prop("id", `btn-${i}-${j}`);
+        }
     }
 
-    // controllo se la riga è già piena o no
-    if (riga != -1) {
-      // creo la nuova pedina
-      let pedina = $("<div>");
-      pedina.appendTo(_wr);
-      pedina.css({"background-color": turno,
-                    "position": "absolute",
-                    "top": -60,  // se omentto i "px" poso anche omettere le ""
-                    "left": colonna*60+5
-                  });
-      _header.off("click");
+    function down() {
+        // restituisce l'indice di $(this) all'interno del contenitore
+        let colonna = _header.children("div").index($(this));
+        let riga = RIGHE - 1; // posizione della prima cella libera
 
-      let _turno = turno;  // variabile ausiliaria di turno
+        for (var i = 0; i < RIGHE; i++) {
+            let p = $(`#btn-${i}-${colonna}`);
+            if (p.css("background-color") != GRIGIO) {
+                riga = i - 1;
+                break;
+            }
+        }
 
-      // cambio il turno
-      if (turno == GIALLO)
-        turno = ROSSO;
-      else turno = GIALLO;
+        // controllo se la riga è già piena o no
+        if (riga != -1) {
+            // creo la nuova pedina
+            let pedina = $("<div>");
+            pedina.appendTo(_wr);
+            pedina.css({
+                "background-color": turno,
+                "position": "absolute",
+                "top": -60, // se omentto i "px" poso anche omettere le ""
+                "left": colonna * 60 + 5
+            });
+            _header.off("click");
 
-      // triggheriamo l'evento sulla pedina corrente
-      // così la pedina cambia colore anche se non mi muovo
-      $(this).trigger("mouseenter");
+            let _turno = turno; // variabile ausiliaria di turno
 
-      // animazione
-      pedina.animate({"top": riga*60+5}, 200*(riga+1), function() {
-        $(`#btn-${riga}-${colonna}`).css({"background-color": _turno})
-        _header.on("click", "div", down);
-      });
+            // cambio il turno
+            if (turno == GIALLO)
+                turno = ROSSO;
+            else turno = GIALLO;
+
+            // triggheriamo l'evento sulla pedina corrente
+            // così la pedina cambia colore anche se non mi muovo
+            $(this).trigger("mouseenter");
+
+            // animazione
+            pedina.animate({ "top": riga * 60 + 5 }, 200 * (riga + 1), function() {
+                $(`#btn-${riga}-${colonna}`).css({ "background-color": _turno })
+                _header.on("click", "div", down);
+            });
+        } else {
+            alert("Mossa non valida")
+        }
     }
-    else {
-      alert("Mossa non valida")
-    }
-  }
 
 });
--- JQuery/Es 015 - Stage/index.js
+++ JQuery/Es 015 - Stage/index.js
@@ -1,148 +1,146 @@
 "use strict"
 
- $(document).ready(function(){
+$(document).ready(function() {
 
-   let _wrapper = $("#wrapper");
-   let _timer = $("#timer");
-   let _header = $("#header");
-   let _mainSection = $("#mainSection");
+    let _wrapper = $("#wrapper");
+    let _timer = $("#timer");
+    let _header = $("#header");
+    let _mainSection = $("#mainSection");
 
-   animazioneHeader();
+    animazioneHeader();
 
-   // ---------------------------------------------------- funzioni
+    // ---------------------------------------------------- funzioni
 
-   function animazioneHeader() {
-     _header.animate({"width": 60*15, "height": 6*15, "font-size": 2*15, "line-height": 6*15}, 1500, creaElementi);
-   }
+    function animazioneHeader() {
+        _header.animate({ "width": 60 * 15, "height": 6 * 15, "font-size": 2 * 15, "line-height": 6 * 15 }, 1500, creaElementi);
+    }
 
-   // creazione elementi e gestione timer
-   function creaElementi() {
-     let cont = 0;
+    // creazione elementi e gestione timer
+    function creaElementi() {
+        let cont = 0;
 
-     for (var item1 of elencoDomande) {
-       // creazione fieldset
-       let fieldset = $("<fieldset>");
-       fieldset.addClass("fieldset");
-       fieldset.appendTo(_mainSection);
+        for (var item1 of elencoDomande) {
+            // creazione fieldset
+            let fieldset = $("<fieldset>");
+            fieldset.addClass("fieldset");
+            fieldset.appendTo(_mainSection);
 
-       // creazione legend
-       let legend = $("<legend>");
-       legend.text(item1.argomento);
-       legend.css({"color": "blue", "font-size": 12});
-       legend.appendTo(fieldset);
+            // creazione legend
+            let legend = $("<legend>");
+            legend.text(item1.argomento);
+            legend.css({ "color": "blue", "font-size": 12 });
+            legend.appendTo(fieldset);
 
-       // creazione domande e radio button
-       for (var item2 of item1.domande) {
-         cont++;
+            // creazione domande e radio button
+            for (var item2 of item1.domande) {
+                cont++;
 
-         let domanda = $("<p>");
-         domanda.text(item2.domanda);
-         domanda.appendTo(fieldset);
+                let domanda = $("<p>");
+                domanda.text(item2.domanda);
+                domanda.appendTo(fieldset);
 
-         let radioT = $("<input>");
-         radioT.prop({"type": "radio", "name": `opt${cont}`, "value": "T"});
-         radioT.appendTo(domanda);
+                let radioT = $("<input>");
+                radioT.prop({ "type": "radio", "name": `opt${cont}`, "value": "T" });
+                radioT.appendTo(domanda);
 
-         let lblT = $("<label>");
-         lblT.text("T");
-         lblT.prop("id", "lbl");
-         lblT.appendTo(domanda);
+                let lblT = $("<label>");
+                lblT.text("T");
+                lblT.prop("id", "lbl");
+                lblT.appendTo(domanda);
 
-         let radioF = $("<input>");
-         radioF.prop({"type": "radio", "name": `opt${cont}`, "value": "F"});
-         radioF.appendTo(domanda);
+                let radioF = $("<input>");
+                radioF.prop({ "type": "radio", "name": `opt${cont}`, "value": "F" });
+                radioF.appendTo(domanda);
 
-         let lblF = $("<label>");
-         lblF.text("F");
-         lblF.prop("id", "lbl");
-         lblF.appendTo(domanda);
+                let lblF = $("<label>");
+                lblF.text("F");
+                lblF.prop("id", "lbl");
+                lblF.appendTo(domanda);
 
-         if(item2.risposta == "T"){
-           radioT.prop("risposta", "ok");
-           radioF.prop("risposta", "nok");
-         }
-        else{
-          radioT.prop("risposta", "nok");
-          radioF.prop("risposta", "ok");
+                if (item2.risposta == "T") {
+                    radioT.prop("risposta", "ok");
+                    radioF.prop("risposta", "nok");
+                } else {
+                    radioT.prop("risposta", "nok");
+                    radioF.prop("risposta", "ok");
+                }
+            }
         }
-       }
-     }
 
-     // creazione timer
-     let secondi = $("<span>");
-     let txtSecondi = 0;
-     secondi.text(":0"+txtSecondi);
-     let mySeconds = setInterval(function() {
-       txtSecondi++;
-       if(txtSecondi < 10)
-         secondi.text(":0"+txtSecondi);
-       else
-         secondi.text(":"+txtSecondi);
-       if(txtSecondi==59)
-         txtSecondi=0;
-     }, 1000);
+        // creazione timer
+        let secondi = $("<span>");
+        let txtSecondi = 0;
+        secondi.text(":0" + txtSecondi);
+        let mySeconds = setInterval(function() {
+            txtSecondi++;
+            if (txtSecondi < 10)
+                secondi.text(":0" + txtSecondi);
+            else
+                secondi.text(":" + txtSecondi);
+            if (txtSecondi == 59)
+                txtSecondi = 0;
+        }, 1000);
 
-     let minuti = $("<span>");
-     let txtMin = 0;
-     minuti.text("0"+txtMin);
-     let myMinutes = setInterval(function() {
-       txtMin++;
-       if(txtSecondi < 10)
-         minuti.text("0"+txtMin);
-       else
-         minuti.text(txtMin);
-     }, 60000);
+        let minuti = $("<span>");
+        let txtMin = 0;
+        minuti.text("0" + txtMin);
+        let myMinutes = setInterval(function() {
+            txtMin++;
+            if (txtSecondi < 10)
+                minuti.text("0" + txtMin);
+            else
+                minuti.text(txtMin);
+        }, 60000);
 
-     minuti.appendTo(_timer);
-     secondi.appendTo(_timer);
+        minuti.appendTo(_timer);
+        secondi.appendTo(_timer);
 
-     // creazione bottone invia
-     let btn = $("<input>")
-     btn.prop({"type": "button", "value": "Invia"});
-     btn.addClass("invia");
-     btn.appendTo(mainSection)
+        // creazione bottone invia
+        let btn = $("<input>")
+        btn.prop({ "type": "button", "value": "Invia" });
+        btn.addClass("invia");
+        btn.appendTo(mainSection)
 
-     btn.on("click", function () {
-       $(this).prop("disabled", true);
-       $(this).css({ "background-color": "#CCC" })
-       clearInterval(mySeconds);
-       clearInterval(myMinutes);
-       calcolaPunti();
-     });
+        btn.on("click", function() {
+            $(this).prop("disabled", true);
+            $(this).css({ "background-color": "#CCC" })
+            clearInterval(mySeconds);
+            clearInterval(myMinutes);
+            calcolaPunti();
+        });
 
-     // timer >= 2min
-     if(txtMin == 2){
-       clearInterval(mySeconds);
-       clearInterval(myMinutes);
-       btn.prop("disabled", true);
-       btn.css({ "background-color": "#CCC" });
-       calcolaPunti();
-     }
-   }
+        // timer >= 2min
+        if (txtMin == 2) {
+            clearInterval(mySeconds);
+            clearInterval(myMinutes);
+            btn.prop("disabled", true);
+            btn.css({ "background-color": "#CCC" });
+            calcolaPunti();
+        }
+    }
 
-   function calcolaPunti() {
+    function calcolaPunti() {
 
-     let punti=0;
-     let radio = _mainSection.find("input[type=radio]");
-     let lbl = _mainSection.find("label");
+        let punti = 0;
+        let radio = _mainSection.find("input[type=radio]");
+        let lbl = _mainSection.find("label");
 
-     for (var i = 0; i < radio.length; i++) {
-       if(radio.eq(i).prop("checked") && radio.eq(i).prop("risposta") == "ok"){
-         punti++;
-       }
-       else if (radio.eq(i).prop("checked") && radio.eq(i).prop("risposta") != "ok") {
-         punti=punti-0.25;
-         lbl.eq(i).css({"color": "red"});
-       }
-     }
-     alert(`Hai totalizzato ${punti} punti`);
-   }
+        for (var i = 0; i < radio.length; i++) {
+            if (radio.eq(i).prop("checked") && radio.eq(i).prop("risposta") == "ok") {
+                punti++;
+            } else if (radio.eq(i).prop("checked") && radio.eq(i).prop("risposta") != "ok") {
+                punti = punti - 0.25;
+                lbl.eq(i).css({ "color": "red" });
+            }
+        }
+        alert(`Hai totalizzato ${punti} punti`);
+    }
 
- });
+});
 
 
 
 // Una semplice funzione per aggiungere uno 0 davanti ad un numero < 10
 function pad(number) {
-     return (number < 10 ? '0' : '') + number;
+    return (number < 10 ? '0' : '') + number;
 }

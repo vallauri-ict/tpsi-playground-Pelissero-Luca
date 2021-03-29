4b688e2f17e94ca793d783e4098d32eb25d1bcd3
Luca <l.pelissero.1360@vallauri.edu>
2021-03-29 10:09:42 +0200

    da fare

---
+++ GeoLocation/Es 005 - percorso/index.js
@@ -0,0 +1,26 @@
+﻿"use strict"
+
+$(document).ready(function(){
+	let header =$("#header")
+	let button = header.children("input")
+	let partenza = header.find("input").eq(0)
+	let arrivo = header.find("input").eq(1)
+	let wrapper = $("#wrapper") 
+	let map =  wrapper.children(".map")[0]     // js
+	let panel= wrapper.children(".panel")[0]   // js
+	let msg =  wrapper.children(".msg") 
+
+
+	button.on("click", function(){
+		if (partenza.val() == "" && arrivo.val() == "") 
+			alert("Prego compilare i campi di partenza e arrivo")
+		else{	
+		
+		}
+	})
+
+	
+
+
+
+});

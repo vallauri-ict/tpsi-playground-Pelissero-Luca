8122b2928f2462ebf0421860c9bae8fdd90fcc67
Luca <l.pelissero.1360@vallauri.edu>
2021-04-30 15:45:48 +0200

    Esercizi aggiornati

--- PHP/Es 001 - prova/index.php
+++ PHP/Es 001 - prova/index.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="it">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <head>
+ <meta charset="UTF-8"/>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> PHP </title>
-    <link rel="stylesheet" href="index.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <script src="index.js"></script>
+	<script> src="https://code.jqeury.com/jquery-3.6.0.js"
+	integrity="sha256-H+K7U5CnXI1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+	crossorigin="anonymus"</script>
+	<link rel="stylesheet" href="index.css"/>
+	<script type="application/javascript" src="index.js"> </script>
 </head>
 
-<body>
-    <h1> Esercizio numero 1 PHP </h1>
+	<body>
+		<h1>Esercizio 1</h1>
+		<?php
+			$nome = "pippo";
+			echo ("Il mio nome è $nome <br>");
+			visualizza ($nome);
+			function visualizza($nome) {
+				echo ("<p style='font-weight:bold''>Il mio nome è $nome </p>");
+			}
+		?>
 
-    <?php
-        $nome = "pippo";
-        echo ("Il mio nome &egrave; $nome <br>");
-        echo("\n\t");
-        visualizza ($nome);
-        function visualizza($nome) {
-        echo("<p style='font-weight:bold''>Il mio nome &egrave; $nome </p>");
-        }
-    ?>
-    <h1>Contenuto della variabile globale $_SERVER</h1>
-    <?php
-        foreach ($_SERVER as $key => $valore)
-            echo("$key : $valore <br>\n");
-    ?>
-    <h1>Informazioni sulla configurazione di XAMPP</h1>
-    <?php
-        echo(phpinfo());
-    ?>
-</body>
+		<h1> Contenuto della variabile globale $_SERVER</h1>
+		<?php
+			foreach ($_SERVER as $key => $valore) 
+				echo ("$key .  : . $valore . <br>\n");
+		?>
 
+		<h1> Info sulla configurazione di Xampp</h1>
+		<?php
+			echo (phpinfo());
+		?>
+	</body>
 </html>

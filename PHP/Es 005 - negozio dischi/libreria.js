2812d4945bef6a0a64dba52b4551ae3c1eacb5bd
Luca <l.pelissero.1360@vallauri.edu>
2021-05-19 18:44:24 +0200

    Esercizio 5 aggiornato e terminato

--- PHP/Es 005 - negozio dischi/libreria.js
+++ PHP/Es 005 - negozio dischi/libreria.js
@@ -1,33 +1,44 @@
 "use strict";
 
-function inviaRichiesta(method, url, parameters = {}) {
-    let contentType;
-    if (method.toUpperCase() == "GET")
-        contentType = "application/x-www-form-urlencoded;charset=utf-8";
-    else {
-        contentType = "application/json; charset=utf-8"
+const PHP = true;
+
+function inviaRichiesta(method, url, parameters={}) {
+	let contentType;
+	// urlencoded --> formato nome = value
+	if(method.toUpperCase()=="GET" || PHP)
+		contentType="application/x-www-form-urlencoded;charset=utf-8";
+	else{
+		// se la chiamata è post serializza i dati e li invia
+		contentType = "application/json; charset=utf-8"
         parameters = JSON.stringify(parameters);
-    }
+	}
     return $.ajax({
-        "url": URL + url,
-        "data": parameters,
-        "type": method,
-        "contentType": contentType,
-        "dataType": "json", // default      
-        "timeout": 5000, // default 
+        // non serve alcun tipo di concatenamento in quanto
+        // il browser, automaticamente, richiede il file allo stesso
+        // server che ci ha fornito la pagina
+        "url": url,
+		"data": parameters,
+		"type": method,   
+		"contentType": contentType, 
+        "dataType": "json",   
+        "timeout": 5000,      // default
     });
 }
-
 
 function errore(jqXHR, text_status, string_error) {
     if (jqXHR.status == 0)
         alert("Connection Refused or Server timeout");
-    else if (jqXHR.status == 200)
+        // status significa che la risposta è arrivata correttamente
+        // ma è andato in errore in quanto il client non è riuscito a parsificare i dati
+	else if (jqXHR.status == 200)
         alert("Formato dei dati non corretto : " + jqXHR.responseText);
+        // un qualunque numero diverso da 200 significa errore, se il server
+        // rimanda in dietro un numero diverso da 200 mostriamo semplicemente
+        // l'errore che ci ritorna    
     else
         alert("Server Error: " + jqXHR.status + " - " + jqXHR.responseText);
 }
 
-function generaNumero(a, b) {
-    return Math.floor((b - a + 1) * Math.random()) + a;
+function generaNumero(a, b){
+	return Math.floor((b-a+1)*Math.random()) + a;
 }
--- PHP/Es 005 - negozio dischi/server/elencoDischi.php
+++ PHP/Es 005 - negozio dischi/server/elencoDischi.php
@@ -1,21 +1,22 @@
 <?php
-    // prima dell'aperto e dopo il chiuso script php, non ci deve essere nulla; neanche uno spazio.
-    // eventuali caratteri scritti al di fuori dello script verrebbero inviati al cliente
-    //  e potrebbero causare problemi.
-    // echo('{"ris": "ok"}');
+    // prima del <?php e del chiuso script php
+    // non ci deve essere nulla, neanche un carattere di tipo spazio
+    // eventuali caratteri scritti al di fuori dello script verrebbero inviati e potrebbero causare problemi
+    //echo('{"ris":"ok"}');
 
     header('Content-type: text/html; charset=utf-8');
     require("php-mysqli.php");
-
-    // step 1: lettura parametri
-    // null
-    // step 2: conessione
+    // step 1:
+    // no parametri
+    // step 2 connessione:
     $con = _openConnection("4b_dischi");
-    // step 3: esecuzione query
+    // step 3 esecuzione query:
     $sql = "SELECT * FROM `dischi`";
-    $rs = _eseguiQuery($con, $sql);
-    // step 4: invio dei dati al client
+    $rs = _eseguiQuery($con,$sql);
+    // step 4 invio dei dati al client:
     echo(json_encode($rs));
-    // step 5: chiusura connessione
+
+    //step 5 chiusura connessione:
     $con->close();
+
 ?>

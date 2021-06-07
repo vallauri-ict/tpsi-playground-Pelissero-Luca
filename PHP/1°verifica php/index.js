22cda0046b45bb5d2a39743bf1ebf7d847bbb303
Luca <l.pelissero.1360@vallauri.edu>
2021-06-07 19:20:21 +0200

    Updated Es 7 PHP e verifica PHP

--- PHP/1°verifica php/index.js
+++ PHP/1°verifica php/index.js
@@ -115,7 +115,8 @@ $(document).ready(function() {
                         })
                     } else
                         alert("Stringa immessa non valida");
-                });
+                }
+            );
         })
     }
 });
